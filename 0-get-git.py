fa66cc45935a35b2609abd321e4e5e8c849ce636
poshaw <poadshaw@gmail.com>
2023-01-06 15:05:05 -0600

    20230106_1505

--- 0-get-git.py
+++ 0-get-git.py
@@ -1,35 +1,6 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-from getpass import getpass
-from myutils import bash
-from shlex import split
-from shutil import copy as shutilcopy
-from subprocess import Popen, PIPE
-
-user = 'phil'
-# password = getpass(prompt = 'Enter root password: ')
-# password = bytes(password, 'utf-8')
-
-# update the system
-bash('/usr/bin/apt update')		
-bash('/usr/bin/apt upgrade -y')		
-
-# install packages
-bash('/usr/bin/apt install -y git')		
-
-# set username
-bash('/usr/bin/git config --global user.name "poshaw"')		
-bash('/usr/bin/git config --global user.name')		
-
-# set email
-bash('/usr/bin/git config --global user.email "poadshaw@gmail.com"')		
-bash('/usr/bin/git config --global user.email')		
-
-# name default branch for new repos
-bash('/usr/bin/git config --global init.defaultBranch "master"')
-=======
 from myutils import bash
 from shlex import split
 from sys import exit argv
@@ -60,12 +31,5 @@ def main(args):
 	configure_git()
 	return 0
 
-
-
-
-
-
-
 if __name__ == "__main__":
 	sys.exit(main(sys.argv[1:]))
->>>>>>> 21c6294054099d10856f94bf71c3826c5ed135bd
