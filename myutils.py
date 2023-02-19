648ec258e1cc3d72811a18126f6f415ca4dee9e8
Phil Shaw <poadshaw@gmail.com>
2023-02-19 00:30:10 -0600

    removed docstring

--- myutils.py
+++ myutils.py
@@ -5,31 +5,6 @@ from shlex import split
 from subprocess import Popen, PIPE
 
 def bash(command, *, input=None, stdin=None, stdout=None, stderr=None):
-    """
-    Execute a shell command and return its output as a string.
-
-    Args:
-        command (str): The command to execute.
-        input (bytes, optional): The input to feed to the command's stdin. Defaults to None.
-        stdin (file-like object, optional): The stdin to use for the command. Defaults to None.
-        stdout (file-like object, optional): The stdout to use for the command. Defaults to None.
-        stderr (file-like object, optional): The stderr to use for the command. Defaults to None.
-
-    Returns:
-        str: The output of the command, with trailing newline characters removed.
-
-    Raises:
-        TypeError: If input is provided but is not a bytes object.
-
-    Usage:
-        >>> output = bash('ls -la')
-        >>> print(output)
-
-    This function is intended for use in a Unix-like environment with a Bash shell.
-    For more information, see the project documentation at https://github.com/myproject/mymodule.
-    """
-
-
     if input is not None:
         stdin=PIPE
         if not isinstance(input, bytes):
