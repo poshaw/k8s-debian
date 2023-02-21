5a6c469a3f8b9b637294493d880caeba8c274726
Phil Shaw <phil.shaw@gmx.com>
2023-02-21 12:44:41 -0600

    20230221_1244

--- 0-get-git.py
+++ 0-get-git.py
@@ -1,12 +1,11 @@
-#! /usr/bin/env python3
+#! /usr/bin/env python3 -B
 # coding: utf-8
 
 from myutils import bash
-from os import chmod, listdir, mkdir, path
-from shlex import split
-from shutil import copytree
+import os
+import shutil
 from subprocess import PIPE
-from sys import exit, argv
+import sys
 
 user = 'phil'
 email = 'poadshaw@gmail.com'
@@ -14,36 +13,37 @@ keydir = '/mnt/shared/ssh'
 
 def install_git():
     # update the system
-    bash('/usr/bin/apt update')		
-    bash('/usr/bin/apt upgrade -y')		
+    apt = shutil.which('apt')
+    bash(f'{apt} update')		
+    bash(f'{apt} upgrade -y')		
 
     # install packages
-    bash('/usr/bin/apt install -y git openssh-server')	
+    bash(f'{apt} install -y git openssh-server')	
 
 def configure_git():
     # set username
-    bash(f'/usr/bin/git config --global user.name "{user}"')		
+    git = shutil.which('git')
+    bash(f'{git} config --global user.name "{user}"')		
 
     # set email
-    bash(f'/usr/bin/git config --global user.email "{email}"')		
+    bash(f'{git} config --global user.email "{email}"')		
 
     # name default branch for new repos
-    bash('/usr/bin/git config --global init.defaultBranch "master"')
+    bash(f'{git} config --global init.defaultBranch "master"')
 
 def setup_ssh_to_github():
     sshdir = f'/home/{user}/.ssh'
-    if not path.exists(sshdir):
-        mkdir(sshdir)
+    if not os.path.exists(sshdir):
+        os.mkdir(sshdir)
     # bash(f'/usr/bin/cp {keydir}/* {sshdir}')
-    copytree(keydir,sshdir, dirs_exist_ok=True)
+    shutil.copytree(keydir,sshdir, dirs_exist_ok=True)
     uid = int(bash(f'/usr/bin/id -u {user}', stdout=PIPE))
     gid = int(bash(f'/usr/bin/id -g {user}', stdout=PIPE))
     bash(f'/usr/bin/chown --recursive {uid}:{gid} {sshdir}')
-    chmod(sshdir,0o700)
-    for file in listdir(sshdir):
-        chmod(path.join(sshdir,file),0o644)
-    chmod(path.join(sshdir, 'id_ed25519'),0o600)
-
+    os.chmod(sshdir,0o700)
+    for file in os.listdir(sshdir):
+        os.chmod(os.path.join(sshdir,file),0o644)
+    os.chmod(os.path.join(sshdir, 'id_ed25519'),0o600)
 
 def main(args):
     install_git()
@@ -52,4 +52,4 @@ def main(args):
     return 0
 
 if __name__ == "__main__":
-    exit(main(argv[1:]))
+    sys.exit(main(sys.argv[1:]))
