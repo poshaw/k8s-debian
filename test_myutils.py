e7bcc375b9ea86a7ea29431f75d29c2cd3f34fdb
Phil Shaw <phil.shaw@gmx.com>
2023-02-21 20:46:12 -0600

    20230221_2046

--- test_myutils.py
+++ test_myutils.py
@@ -3,7 +3,6 @@
 
 import unittest
 import myutils
-from subprocess import PIPE
 
 class TestMyutils(unittest.TestCase):
     @classmethod
@@ -22,24 +21,22 @@ class TestMyutils(unittest.TestCase):
 
     def test_bash_write_to_stdout(self):
         print("\nTesting writing to stdout:")
-        twoLines = "  This line should print twice!"
-        print(twoLines)
-        myutils.bash(f'/usr/bin/echo "{twoLines}"')
+        result = myutils.bash(f'/usr/bin/echo "{self.string}"')
+        print(result)
 
     def test_bash_write_to_pipe(self):
         print("\nTesting writing to pipe:")
-        output = myutils.bash(f'/usr/bin/echo "{self.string}"', stdout=PIPE)
+        output = myutils.bash(f'/usr/bin/echo "{self.string}"')
         self.assertEqual(self.string, output)
 
     def test_bash_input_from_stdin(self):
         print("\nTesting input from pipe:")
-        self.sb = bytes(self.string, 'utf-8')
-        output = myutils.bash('/usr/bin/cat', input=self.sb, stdout=PIPE)
+        output = myutils.bash('/usr/bin/cat', input=self.string)
         self.assertEqual(self.string, output)
 
 
-        with self.assertRaises(TypeError):
-            myutils.bash('/usr/bin/cat', input=self.string)
+        # with self.assertRaises(TypeError):
+            # myutils.bash('/usr/bin/cat', input=self.string)
         
 
 if __name__ == '__main__':
