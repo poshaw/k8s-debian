7d1a4a4728a1e9dbd4b181b3582152e3cba4c803
Phil Shaw <poadshaw@gmail.com>
2023-02-22 16:05:34 -0600

    Update 2-install-k8s.py

--- 2-install-k8s.py
+++ 2-install-k8s.py
@@ -1,35 +1,62 @@
 #! /usr/bin/env -S python3 -B
 # coding: utf-8
 
+from subprocess import run, PIPE
 from myutils import bash
+import os
+import shlex
 import shutil
-import subprocess
 import sys
+from urllib.request import urlopen
 
 def check_swap():
     cmd = "free | awk '/^Swap:/ {exit !($2+$3)}'"
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = run(cmd, shell=True, stdout=PIPE, stderr=PIPE, text=True)
     return result.returncode == 0 and int(result.stdout.strip()) > 0
 
 def load_kernel_modules():
     modprobe = shutil.which('modprobe')
     bash(f"{modprobe} overlay")
     bash(f"{modprobe} br_netfilter")
-
-    # load modules at each boot
     path = '/etc/modules-load.d'
     src = 'k8s-modules.conf'
     dst = os.path.join(path,src)
     shutil.copyfile(src, dst)
 
-    # set k8s kernel parameters
-    path = '/etc/sysctl.d'
-    src = 'k8s.conf'
-    dst = os.path.join(path,src)
-    shutil.copyfile(src, dst)
+def download_key(url):
+    with urlopen(url) as response:
+        return response.read()
 
-    sysctl = shutil.which('sysctl')
-    bash(f"{sysctl} --system")
+def add_key(key):
+    cmd = shlex.split(f"{shutil.which('gpg')} --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg")
+    try:
+        result = run(cmd, input=key, stdout=PIPE, stderr=PIPE, check=True)
+    except CalledProcessError as e:
+        print(f"Error: {e.stderr.decode().strip()}")
+        sys.exit(1)
+
+    return result.stdout.decode().strip()
+
+def main(argv):
+    # Check that script is running with root privleges
+    if os.geteuid() != 0:
+        print("This script must be run as a privileged user or with the sudo command.")
+        return 1
+
+    # check for swap
+    if check_swap():
+        print("Swap is enabled on this system.  Disable swap and re-run this script.")
+        return 1
+    else:
+        print("No Swap is enabled, you're good to proceed!")
+
+    load_kernel_modules()
+
+    url = "https://download.docker.com/linux/debian/gpg"
+    key = download_key(url)
+    add_key(key)
+
+    return 0
     
 
 def main(argv):
