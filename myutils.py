665e657e2f7082f88e1abb7293bec0f90fea8895
Phil Shaw <poadshaw@gmail.com>
2023-03-06 17:32:06 -0600

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -3,12 +3,16 @@ import subprocess
 
 def runc(cmd, input=None):
     cmd_list = shlex.split(cmd)
+    stdin = subprocess.PIPE if input else None
+    if isinstance(input, str):
+        input = input.encode('utf-8')
     result = subprocess.run(
         cmd_list,
-        input=input.encode('utf-8') if input else None,
+        input=input,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         universal_newlines=True,
-        check=True
+        check=True,
+        stdin=stdin
     )
     return result.stdout, result.stderr
