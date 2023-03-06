e62e8e1845a1de11eb812793cc536cfe6c3b54fe
Phil Shaw <poadshaw@gmail.com>
2023-03-06 15:29:33 -0600

    Update myutils.py

--- myutils.py
+++ myutils.py
@@ -1,12 +1,17 @@
 #! /usr/bin/env -S python3 -B
 # coding: utf-8
 
-from subprocess import run, PIPE
-from shlex import split
+import shlex
+import subprocess
 
-def bash(command, *, input=None):
-    if input is not None:
-        result = run(split(command), input=input, stdout=PIPE, stderr=PIPE, text=True, check=True)
-    else:
-        result = run(split(command), stdout=PIPE, stderr=PIPE, text=True, check=True)
-    return result.stdout.rstrip()
+def runc(cmd, input=None):
+    cmd_list = shlex.split(cmd)
+    result = subprocess.run(
+        cmd_list,
+        input=input.encode('utf-8') if input else None,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        check=True
+    )
+    return result.stdout, result.stderr
