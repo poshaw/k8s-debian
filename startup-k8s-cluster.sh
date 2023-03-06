3067f2673c54072ee8c2056da5da2288b462ebbf
Phil Shaw <poadshaw@gmail.com>
2023-03-06 14:46:53 -0600

    Update startup-k8s-cluster.sh

--- startup-k8s-cluster.sh
+++ startup-k8s-cluster.sh
@@ -1,51 +1,50 @@
 #! /usr/bin/env python3
-2  # coding: utf-8
-3
-4  import getpass
-5  import subprocess
-6  import os
-7  import sys
-8
-9  user = getpass.getuser()
-10 password = getpass.getpass(prompt='Enter sudo password: ')
-11 nodes = ['km1.lan', 'kw1.lan']
-12 CIDR = '10.100.0.0/16'
-13 kubedir = os.path.join(os.path.expanduser('~'), '.kube')
-14 kubeconf = os.path.join(os.path.expanduser('~'), '.kube', 'config')
-15 yamldir = os.path.join(os.path.expanduser('~'), 'yaml')
-16 calico = os.path.join(yamldir, 'calico.yaml')
-17 sudo = '/usr/bin/sudo -S'
-18
-19 def reset_cluster():
-20     for node in nodes[1:]:
-21         # reset node
-22         subprocess.run(split(f'/usr/bin/ssh {user}@{node} "{sudo} /usr/bin/kubeadm reset --force"'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-23
-24     subprocess.run(split(f'{sudo} /usr/bin/rm -rf {kubedir}'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-25
-26 def initialize_master():
-27     subprocess.run(split(f'{sudo} /usr/bin/kubeadm config images pull'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-28     subprocess.run(split(f'{sudo} /usr/bin/kubeadm init --control-plane-endpoint={nodes[0]}:6443 --pod-network-cidr={CIDR}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-29     subprocess.run(split(f'/usr/bin/mkdir {kubedir}'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-30     subprocess.run(split(f'{sudo} /usr/bin/kubeadm config images pull'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-31     uid = os.getuid()
-32     gid = os.getgid()
-33     subprocess.run(split(f'{sudo} /usr/bin/cp -i /etc/kubernetes/admin.conf {kubeconf}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-34     subprocess.run(split(f'{sudo} /usr/bin/chown {uid}:{gid} {kubeconf}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
-35
-36 def main(args):
-37     cleanup()
-38     initialize_master()
-39
-40     subprocess.run(split('/usr/bin/kubectl cluster-info'))
-41
-42 if __name__ == "__main__":
-43     sys.exit(main(sys.argv[1:]))
+# coding: utf-8
 
+import getpass
+import subprocess
+import os
+import sys
 
+user = getpass.getuser()
+password = getpass.getpass(prompt='Enter sudo password: ')
+nodes = ['km1.lan', 'kw1.lan']
+CIDR = '10.100.0.0/16'
+kubedir = os.path.join(os.path.expanduser('~'), '.kube')
+kubeconf = os.path.join(os.path.expanduser('~'), '.kube', 'config')
+yamldir = os.path.join(os.path.expanduser('~'), 'yaml')
+calico = os.path.join(yamldir, 'calico.yaml')
+sudo = '/usr/bin/sudo -S'
 
+def reset_cluster():
+    for node in nodes[1:]:
+        # reset node
+        subprocess.run(split(f'/usr/bin/ssh {user}@{node} "{sudo} /usr/bin/kubeadm reset --force"'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
 
+    subprocess.run(split(f'{sudo} /usr/bin/rm -rf {kubedir}'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
 
+def initialize_master():
+    subprocess.run(split(f'{sudo} /usr/bin/kubeadm config images pull'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    subprocess.run(split(f'{sudo} /usr/bin/kubeadm init --control-plane-endpoint={nodes[0]}:6443 --pod-network-cidr={CIDR}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    subprocess.run(split(f'/usr/bin/mkdir {kubedir}'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    subprocess.run(split(f'{sudo} /usr/bin/kubeadm config images pull'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    uid = os.getuid()
+    gid = os.getgid()
+    subprocess.run(split(f'{sudo} /usr/bin/cp -i /etc/kubernetes/admin.conf {kubeconf}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+    subprocess.run(split(f'{sudo} /usr/bin/chown {uid}:{gid} {kubeconf}'), input=password.encode('utf-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True)
+
+def main(args):
+    reset_cluster()
+    initialize_master()
+
+    subprocess.run(split('/usr/bin/kubectl cluster-info'))
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
+
+    
+    
+    
 
 
 
