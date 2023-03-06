91d4a4d740cc303a24054fe8d88c0fc0138c8d0b
Phil Shaw <poadshaw@gmail.com>
2023-03-06 16:28:50 -0600

    Update startup-k8s-cluster.sh

--- startup-k8s-cluster.sh
+++ startup-k8s-cluster.sh
@@ -2,12 +2,15 @@
 # coding: utf-8
 
 import getpass
-from myutils import runc
+import logging
 import os
 import shlex
 import shutil
 import subprocess
 import sys
+from myutils import runc
+
+logging.basicConfig(level=logging.INFO)
 
 user = getpass.getuser()
 password = getpass.getpass(prompt='Enter sudo password: ')
@@ -17,105 +20,125 @@ kubedir = os.path.join(os.path.expanduser('~'), '.kube')
 kubeconf = os.path.join(os.path.expanduser('~'), '.kube', 'config')
 yamldir = os.path.join(os.path.expanduser('~'), 'yaml')
 calico = os.path.join(yamldir, 'calico.yaml')
-sudo = '/usr/bin/sudo -S'
 
-def reset_cluster():
-    for node in nodes[1:]:
+def reset_cluster(workers):
+    for worker in workers:
         # reset node
-        cmd = f'/usr/bin/ssh {user}@{node} "{sudo} /usr/bin/kubeadm reset --force"'
+        cmd = f'ssh {user}@{worker} "sudo -S kubeadm reset --force"'
         stdout, stderr = runc(cmd, input=password)
         if stderr:
-            print(f'Error resetting {node}: {stderr}')
-
-    cmd = f'{sudo} /usr/bin/rm -rf {kubedir}'
+            logging.error(f'Error resetting {worker}: {stderr}')
+        else:
+            logging.info(f'{worker} reset successfully.')
+            
+    cmd = f'rm -rf {kubedir}'
     stdout, stderr = runc(cmd)
     if stderr:
-        print(f'Error cleaning up {kubedir}: {stderr}')
+        logging.error(f'Error cleaning up {kubedir}: {stderr}')
+    else:
+        logging.info(f'{kubedir} cleaned up successfully.')
 
-def initialize_master():
-    cmd = f'{sudo} {shutil.which("kubeadm")} config images pull'
+def initialize_master(master):
+    cmd = f'sudo -S kubeadm config images pull'
     stdout, stderr = runc(cmd, input=password)
     if stderr:
-        print(f'Error pulling images: {stderr}')
+        logging.error(f'Error pulling images: {stderr}')
+        return
 
-    cmd = f'{sudo} {shutil.which("kubeadm")} init --control-plane-endpoint={nodes[0]}:6443 --pod-network-cidr={CIDR}'
+    cmd = f'sudo -S kubeadm init --control-plane-endpoint={master}:6443 --pod-network-cidr={CIDR}'
     stdout, stderr = runc(cmd, input=password)
     if stderr:
-        print(f'Error initializing master: {stderr}')
+        logging.error(f'Error initializing master: {stderr}')
+        return
 
-    cmd = f'{shutil.which("mkdir")} {kubedir}'
+    cmd = f'mkdir {kubedir}'
     stdout, stderr = runc(cmd)
     if stderr:
-        print(f'Error creating directory {kubedir}: {stderr}')
+        logging.error(f'Error creating directory {kubedir}: {stderr}')
+        return
 
-    cmd = f'{sudo} {shutil.which("cp")} -i /etc/kubernetes/admin.conf {kubeconf}'
+    cmd = f'sudo -S cp -i /etc/kubernetes/admin.conf {kubeconf}'
     stdout, stderr = runc(cmd, input=password)
     if stderr:
-        print(f'Error copying directory {kubedir}: {stderr}')
+        logging.error(f'Error copying directory {kubedir}: {stderr}')
+        return
 
     uid = os.getuid()
     gid = os.getgid()
     
-    cmd = f'{sudo} {shutil.which("chown")} {uid}:{gid} {kubeconf}'
+    cmd = f'sudo -S chown {uid}:{gid} {kubeconf}'
     stdout, stderr = runc(cmd, input=password)
     if stderr:
-        print(f'Error changing permission of {kubeconf} to {uid}:{gid}: {stderr}')
+        logging.error(f'Error changing permission of {kubeconf} to {uid}:{gid}: {stderr}')
+        return
+
+def worker_join_cluster(workers, join_command):
+    for worker in workers:
+        # have worker join the cluster
+        cmd = f'ssh {user}@{worker} "sudo -S {join_command}"'
+        stdout, stderr = runc(cmd, input=password)
+        if stderr:
+            logging.error(f'Error joining worker {worker} to the cluster: {stderr}')
     
-def main(args):
-    reset_cluster()
-    initialize_master()
-
-    subprocess.run(split('/usr/bin/kubectl cluster-info'))
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))
-
-    
-    
-    
-
-
-
-def worker_join_cluster():
-    # have worker join the cluster
-    join_command = bash('/usr/bin/kubeadm token create --print-join-command')
-    bash(f'/usr/bin/ssh {user}@{worker} "{sudo} {join_command}"', input=password)
-
 def setup_calico():
-    # downlowd calico.yaml for network config
-    if not path.exists(yamldir):
-        mkdir(yamldir)
-    bash(f'/usr/bin/curl --fail --silent --show-error --location --output {calico} \
-        https://docs.projectcalico.org/manifests/calico.yaml')
+    # Download calico.yaml for network config
+    if not os.path.exists(yamldir):
+        os.mkdir(yamldir)
 
-    bash(f'/usr/bin/cp {calico} {calico}_bak')
+    calico_url = 'https://docs.projectcalico.org/manifests/calico.yaml'
+    calico_file = os.path.join(yamldir, 'calico.yaml')
+    backup_file = calico_file + '_bak'
 
-    with open(calico, 'r') as f:
+    cmd = f'curl --fail --silent --show-error --location --output {calico_file} {calico_url}'
+    stdout, stderr = runc(cmd)
+    if stderr:
+        logging.error(f'Error downloading calico.yaml: {stderr}')
+        return
+
+    try:
+        shutil.copy(calico_file, backup_file)
+    except (FileNotFoundError, PermissionError) as e:
+        logging.error(f'Error creating backup file: {e}')
+        return
+
+    with open(calico_file, 'r') as f:
         lines = f.readlines()
 
-    # regex for a vaild ipv4 address
+    # Regex for a valid ipv4 address
     validip = r"(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])"
-    i=0
+    i = 0
     while i < len(lines):
         if 'CALICO_IPV4POOL_CIDR' in lines[i]:
             lines[i] = lines[i].replace('# ','')
             lines[i+1] = lines[i+1].replace('# ','')
-            lines[i+1] = sub(validip, CIDR[:-3], lines[i+1])
+            lines[i+1] = re.sub(validip, CIDR[:-3], lines[i+1])
             break
         i += 1
 
-    with open(calico, 'w') as file:
+    with open(calico_file, 'w') as file:
         file.writelines(lines)
 
-    bash(f'/usr/bin/kubectl apply -f {calico}')
+    cmd_apply = f'kubectl apply -f {calico_file}'
+    stdout, stderr = runc(cmd_apply)
+    if stderr:
+        logging.error(f'Error applying calico configuration: {stderr}')
+        return
 
+    logging.info('Calico configuration applied successfully.')
+        
 def main(args):
-    reset_cluster()
-    initialize_master()
+    try:
+        reset_cluster(nodes[1:])
+        initialize_master(nodes[0])
+        join_command = runc('shutil.which("kubeadm")} token create --print-join-command')[0].strip()
+        worker_join_cluster(nodes[1:], join_command)
 
-    bash('/usr/bin/kubectl cluster-info')
+        runc(f'{shutil.which("kubectl")} cluster-info')
+    except Exception as e:
+        logging.exception(e)
 
-    worker_join_cluster()
 
-    setup_calico()
-    bash('/usr/bin/kubectl get nodes')
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    sys.exit(main(sys.argv[1:]))
